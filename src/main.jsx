99c3372f1e4cf995d1665ccb9e331dba3f6c4fe0
mediaphish <mediaphish@users.noreply.github.com>
2025-08-25 20:24:22 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Plan2TasksWizard from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Plan2TasksWizard />
+    <App />
   </React.StrictMode>
-)
+);
